a32413c729ec389f9f27585c9a796bfde0708268
manurubymany <119502036+manurubymany@users.noreply.github.com>
2026-01-27 19:39:21 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Garante que o index.html seja servido na rota raiz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Shadow Of Mystery (Client-Side) running on port ${PORT}`);
