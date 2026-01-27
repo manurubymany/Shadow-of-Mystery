5f34a5ae9c1072f68795f2f173a61760116827e4
manurubymany <119502036+manurubymany@users.noreply.github.com>
2026-01-27 19:19:05 -0300

    Delete iniciar_jogo.bat

--- iniciar_jogo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Shadow Of Mystery Server
-cd /d "%~dp0"
-echo Iniciando o servidor... Acesse http://localhost:3000
-start /b cmd /c "timeout /t 3 >nul & start http://localhost:3000"
-npm start
-pause
